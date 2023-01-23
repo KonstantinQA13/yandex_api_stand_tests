85e0bdfe18ac0822f95657a8704645b2b9976f27
Шарпань Константин Викторович <Sharpan.K@mfua.ru>
2023-01-23 17:00:14 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://5e6b97c3-0a89-48fc-a843-5be8765b4039.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
